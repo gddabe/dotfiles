46ccae85d8ab02564cd381bcc286365190c76164
gddabe <gddabe@gmail.com>
2021-09-14 10:09:18 +0800

    added hyper space to throw window

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -25,3 +25,14 @@ hs.hotkey.bind(hyper, "l", function()
   if not win then return end
 win:moveToUnit(hs.layout.right50)
 end)
+
+-- bind hotkey
+hs.hotkey.bind(hyper, "space", function()
+  -- get the focused window
+  local win = hs.window.focusedWindow()
+  -- get the screen where the focused window is displayed, a.k.a. current screen
+  local screen = win:screen()
+  -- compute the unitRect of the focused window relative to the current screen
+  -- and move the window to the next screen setting the same unitRect
+  win:move(win:frame():toUnitRect(screen:frame()), screen:next(), true, 0)
+end)
